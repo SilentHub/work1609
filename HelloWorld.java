fcf9fbb236cddd85a6ad420e71bb6aa01ce93188
Суханов Александр Денисович <a.suhanov@univ.priv>
2021-09-16 11:43:43 +0300

    Config

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package com.company;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.printf("Hello World\n");
 	System.out.printf("New Feature!");	
     }
 }
